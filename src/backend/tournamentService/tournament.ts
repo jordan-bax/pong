f74ad97af0cd1de82f8b20a184c181ec4d53c696
Luuk Van Geffen <lvan-gef@f1r3s20.codam.nl>
2025-07-25 21:24:16 +0200

    Feat: New logic and start impl how game start

--- src/backend/tournamentService/tournament.ts
+++ src/backend/tournamentService/tournament.ts
@@ -1,31 +1,11 @@
-import { Tournament } from "./schemas/tournamentInterface"
+import { Tournament, TournamentObj } from "./schemas/tournamentInterface"
 
 class TournamentService {
-    private tournaments: Tournament[] = []
-    private runningTournaments: Tournament[] = []
+    private tournaments: TournamentObj[] = []
+    private runningTournaments: TournamentObj[] = []
     private nextID: number = 1
     private checkInterval: NodeJS.Timeout | null = null
     private isChecking: boolean = false
-
-    async create(name: string, description: string, playerCount: number, userID: number, lockTime: number | undefined): Promise<Tournament> {
-        if (lockTime === undefined || lockTime < 1 || lockTime > 3600) {
-            lockTime = 60
-        }
-
-        const newTournament: Tournament = {
-            id: this.nextID++,
-            name,
-            description,
-            playerCount,
-            players: [userID],
-            isLocked: false,
-            lockTime: Math.round(Date.now() + (lockTime * 1000))
-        }
-
-        this.tournaments.push(newTournament)
-        this.startChecker()
-        return Promise.resolve(newTournament)
-    }
 
     private startChecker(): void {
         if (!this.checkInterval && this.tournaments.length > 0) {
@@ -61,60 +41,119 @@ class TournamentService {
         this.isChecking = false
     }
 
+    async create(name: string, description: string, playerCount: number, userID: number, lockTime: number | undefined): Promise<Tournament> {
+        if (lockTime === undefined || lockTime < 1) {
+            lockTime = 60
+        }
+
+        if (lockTime > 3600) {
+            lockTime = 3600
+        }
+
+        const newTournament: TournamentObj = {
+            rounds: 0,
+            currentRound: 0,
+            nextMatchs: [],
+            winner: -1,
+            isRunning: false,
+            lockTime: Math.round(Date.now() + (lockTime * 1000)),
+            currentPlayers: [],
+            tournament: {
+                id: this.nextID++,
+                name,
+                description,
+                playerCount,
+                players: [userID],
+                lockTime: Math.round(Date.now() + (lockTime * 1000))
+            }
+        }
+
+        this.tournaments.push(newTournament)
+        this.startChecker()
+        return newTournament.tournament
+    }
+
     async getByID(id: number): Promise<Tournament | undefined> {
-        return Promise.resolve(this.tournaments.find(tournaments => tournaments.id === id))
+        const tournament = this.tournaments.find(t => t.tournament.id === id)
+        if (tournament === undefined) {
+            return tournament
+        }
+
+        return tournament.tournament
     }
 
     async join(tournamentID: number, playerID: number): Promise<number> {
-        const tournament = this.tournaments.find(tournaments => tournaments.id === tournamentID)
+        const tournament = this.tournaments.find(t => t.tournament.id === tournamentID)
         if (tournament === undefined) {
-            return Promise.resolve(1)
+            return 1
         }
 
-        if (tournament.players.length === tournament.playerCount) {
-            return Promise.resolve(2)
+        if (tournament.tournament.players.length === tournament.tournament.playerCount) {
+            return 2
         }
 
-        if (tournament.players.includes(playerID)) {
-            return Promise.resolve(3)
+        if (tournament.tournament.players.includes(playerID)) {
+            return 3
         }
 
-        tournament.players.push(playerID)
-        return Promise.resolve(0)
+        if (tournament.isRunning) {
+            return 4
+        }
+
+        tournament.tournament.players.push(playerID)
+        return 0
     }
 
     async leave(tournamentID: number, playerID: number): Promise<number> {
-        const tournament = this.tournaments.find(tournaments => tournaments.id === tournamentID)
+        const tournament = this.tournaments.find(t => t.tournament.id === tournamentID)
         if (tournament === undefined) {
-            return Promise.resolve(1)
+            return 1
         }
 
-        const index = tournament.players.indexOf(playerID, 0)
+        const index = tournament.tournament.players.indexOf(playerID, 0)
         if (index === -1) {
-            return Promise.resolve(2)
+            return 2
         }
 
-        tournament.players.splice(index, 1)
-        if (tournament.players.length === 0) {
+        tournament.tournament.players.splice(index, 1)
+        if (tournament.tournament.players.length === 0) {
             const index = this.tournaments.indexOf(tournament, 0)
             this.tournaments.splice(index, 1)
         }
 
-        return Promise.resolve(0)
+        return 0
     }
 
-    async start(tournament: Tournament): Promise<number[]> {
-        const index = this.tournaments.findIndex(t => t.id === tournament.id)
+    async start(t: TournamentObj): Promise<number[]> {
+        // think this make no sense
+        const index = this.tournaments.findIndex(t => t.tournament.id === t.tournament.id)
         if (index === -1) {
-            return Promise.resolve([])
+            return []
         }
 
         this.tournaments.splice(index, 1);
-        tournament.isLocked = true;
-        this.runningTournaments.push(tournament);
+        t.isRunning = true;
+        this.runningTournaments.push(t);
 
-        console.log(`Starting tournament ${tournament.id}`);
-        return Promise.resolve(tournament.players);
+        let aiID = -1
+        for (let index = t.tournament.players.length; index < t.tournament.playerCount; index++) {
+            t.tournament.players.push(aiID)
+            aiID--;
+        }
+
+        t.rounds = Math.ceil(t.tournament.playerCount / 2)
+        t.currentRound = 1
+        t.currentPlayers = [...t.tournament.players]
+        const matches = await this.nextMatch(t.currentPlayers, t.currentPlayers.length)
+
+        console.log(`Starting tournament ${t.tournament.id}`)
+        return t.tournament.players
+    }
+
+    async nextMatch(players: number[], playerCount: number): Promise<number[][]> {
+        // random choice 2 player to play with each other
+        //
+        return [[]]
     }
 }
 

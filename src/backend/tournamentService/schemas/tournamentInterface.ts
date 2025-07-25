2d8b4fa2461fd8d2226b327b4f28f82bf0e190c9
Luuk Van Geffen <lvan-gef@f1r3s20.codam.nl>
2025-07-25 21:23:14 +0200

    Feat: Change structer of the tournament interfaces

--- src/backend/tournamentService/schemas/tournamentInterface.ts
+++ src/backend/tournamentService/schemas/tournamentInterface.ts
@@ -1,9 +1,19 @@
+export interface TournamentObj {
+    rounds: number
+    currentRound: number
+    nextMatchs: number[][]
+    winner: number
+    tournament: Tournament
+    isRunning: boolean
+    lockTime: number
+    currentPlayers: number[]
+}
+
 export interface Tournament {
     id: number
     name: string
     playerCount: number
     description: string
     players: number[]
-    isLocked: boolean
     lockTime: number
 }

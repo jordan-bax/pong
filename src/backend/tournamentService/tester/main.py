73d13cff7886725634d93f462c59f22cfef575b6
Luuk Van Geffen <lvan-gef@f1r3s18.codam.nl>
2025-08-11 20:40:34 +0200

    Update stuff

--- src/backend/tournamentService/tester/main.py
+++ src/backend/tournamentService/tester/main.py
@@ -2,9 +2,9 @@ import requests
 
 def main():
     url = 'http://127.0.0.1:3003/create'
-    resp = requests.post(url=url, json={'name': 'luuk', 'maxPlayers': '8', 'userID': 1, 'lockTime': 10})
+    resp = requests.post(url=url, json={'name': 'luuk', 'maxPlayers': '8', 'userID': 1, 'lockTime': 60})
     data = resp.json()
-    print(f'{data}')
+    print(f'{data}', resp.status_code)
     assert resp.status_code == 201
 
 
@@ -32,6 +32,7 @@ def main():
     # check invalid same ID
     url = 'http://127.0.0.1:3003/join'
     resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
+    print(f'{data}', resp.status_code)
     assert resp.status_code == 400
 
     # valid join
@@ -43,7 +44,6 @@ def main():
     for i in range(data['maxPlayers'] - 2):
         url = 'http://127.0.0.1:3003/join'
         resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': i + 3})
-        print(resp.json())
         assert resp.status_code == 201
 
     # try to join full tournament
@@ -66,36 +66,41 @@ def main():
     resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 200})
     assert resp.status_code == 400
 
-    # # leave tournament
-    # url = 'http://127.0.0.1:3003/leave'
-    # resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
-    # print(f'{resp.json()}')
-    # assert resp.status_code == 201
-    #
-    # # leave tournament with same ID again
-    # url = 'http://127.0.0.1:3003/leave'
-    # resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
-    # assert resp.status_code == 400
-    #
-    # # everyone leave the tournament
-    # for index in range(data['maxPlayers'] - 1):
-    #     url = 'http://127.0.0.1:3003/leave'
-    #     resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': index + 2})
-    #     assert resp.status_code == 201
-    #
-    # # check that tournament is deleted
-    # url = f'http://127.0.0.1:3003/id/{data["id"]}'
-    # resp = requests.get(url=url, params={'id': data['id']})
-    # assert resp.status_code == 400
-    #
-    # # check idle tournaments
-    # url = 'http://127.0.0.1:3003/create'
-    # resp = requests.post(url=url, json={'name': 'luuk', 'description': 'This is a test', 'maxPlayers': '8', 'userID': 1, 'lockTime': 10})
-    # assert resp.status_code == 201
-    #
-    # data = resp.json()
-    # print(f'{data}')
-    #
+    # leave tournament
+    url = 'http://127.0.0.1:3003/leave'
+    resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
+    print(f'{resp.json()}')
+    assert resp.status_code == 201
+
+    # leave tournament with same ID again
+    url = 'http://127.0.0.1:3003/leave'
+    resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
+    assert resp.status_code == 400
+
+    # everyone leave the tournament
+    for index in range(data['maxPlayers'] - 1):
+        url = 'http://127.0.0.1:3003/leave'
+        resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': index + 2})
+        assert resp.status_code == 201
+
+    # leave tournament that is deleted
+    url = 'http://127.0.0.1:3003/leave'
+    resp = requests.post(url=url, json={'tournamentID': data['id'], 'userID': 1})
+    assert resp.status_code == 400
+
+    # check that tournament is deleted
+    url = f'http://127.0.0.1:3003/id/{data["id"]}'
+    resp = requests.get(url=url, params={'id': data['id']})
+    assert resp.status_code == 400
+
+    # check idle tournaments
+    url = 'http://127.0.0.1:3003/create'
+    resp = requests.post(url=url, json={'name': 'luuk', 'description': 'This is a test', 'maxPlayers': '8', 'userID': 1, 'lockTime': 10})
+    assert resp.status_code == 201
+
+    data = resp.json()
+    print(f'{data}')
+
     # # url = 'http://127.0.0.1:3003/idle'
     # # resp = requests.get(url=url)
     # # assert resp.status_code == 200

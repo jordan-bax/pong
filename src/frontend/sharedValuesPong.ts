77990e187186dbbfa4d835611cfbb8f3bc56dc19
Luuk Van Geffen <lvan-gef@f0r1s5.codam.nl>
2025-09-17 19:44:58 +0200

    Cleaning

--- src/frontend/sharedValuesPong.ts
+++ src/frontend/sharedValuesPong.ts
@@ -1,108 +1,108 @@
 export interface gamestateinterface {
-	gameID: number;
-	gameActive: boolean;
-	gamePause: boolean;
-	gametype: string;
-	player1: pcInterface;
-	player2: pcInterface;
-	ball: ballInterface;
+    gameID: number;
+    gameActive: boolean;
+    gamePause: boolean;
+    gametype: string;
+    player1: pcInterface;
+    player2: pcInterface;
+    ball: ballInterface;
 }
 
 export interface basicinfo {
-	paddelspeed: number;
-	ball_dx: number;
-	ball_dy: number;
-	ball_speed: number;
-	ball_staticSpeed: number;
+    paddelspeed: number;
+    ball_dx: number;
+    ball_dy: number;
+    ball_speed: number;
+    ball_staticSpeed: number;
 }
 
 export interface gameWallsInterface {
-	width: number;
-	height: number;
-	wallThickness: number;
-	wallTickness2x: number;
-	egdeThickness: number;
+    width: number;
+    height: number;
+    wallThickness: number;
+    wallTickness2x: number;
+    egdeThickness: number;
 }
 
 export interface scoreInterface {
-	player1Score: number;
-	player2Score: number;
-	player1Name: string;
-	player2Name: string;
+    player1Score: number;
+    player2Score: number;
+    player1Name: string;
+    player2Name: string;
 }
 
 export interface pcInterface {
-	id: number | null;
-	active: boolean;
-	name: string;
-	x: number;
-	y: number;
-	speed: number;
-	height: number;
-	width: number;
-	score: number;
+    id: number | null;
+    active: boolean;
+    name: string;
+    x: number;
+    y: number;
+    speed: number;
+    height: number;
+    width: number;
+    score: number;
 }
 
 export interface ballInterface {
-	x: number;
-	y: number;
-	dx: number;
-	dy: number;
-	speed: number;
-	staticSpeed: number;
-	height: number;
-	width: number;
+    x: number;
+    y: number;
+    dx: number;
+    dy: number;
+    speed: number;
+    staticSpeed: number;
+    height: number;
+    width: number;
 }
 
 export interface aiInterface {
-	reactionTime: number;
+    reactionTime: number;
 }
 
 export const gameWall: gameWallsInterface = {
-	width: 200,
-	height: 200,
-	wallThickness: 10,
-	wallTickness2x: 20,
-	egdeThickness: 15
+    width: 200,
+    height: 200,
+    wallThickness: 10,
+    wallTickness2x: 20,
+    egdeThickness: 15
 };
 
 export var player1Template: pcInterface = {
-	id: null,
-	active: false,
-	name: "Player 1",
-	x: 2,
-	y: 50,
-	speed: 10,
-	height: 25,
-	width: 4,
-	score: 0
+    id: null,
+    active: false,
+    name: "Player 1",
+    x: 2,
+    y: 50,
+    speed: 10,
+    height: 25,
+    width: 4,
+    score: 0
 };
 
 export var player2Template: pcInterface = {
-	id: null,
-	active: false,
-	name: "Player 2",
-	x: 198,
-	y: 50,
-	speed: 10,
-	height: 25,
-	width: 4,
-	score: 0
+    id: null,
+    active: false,
+    name: "Player 2",
+    x: 198,
+    y: 50,
+    speed: 10,
+    height: 25,
+    width: 4,
+    score: 0
 };
 
 export var ai_var: aiInterface = {
-	reactionTime: 100
+    reactionTime: 100
 };
 
 export var ballvarTemplate: ballInterface = {
-	x: 98,
-	y: 98,
-	dx: 2,
-	dy: 5,
-	speed: 2,
-	staticSpeed: 20,
-	height: 4,
-	width: 4
+    x: 98,
+    y: 98,
+    dx: 2,
+    dy: 5,
+    speed: 2,
+    staticSpeed: 20,
+    height: 4,
+    width: 4
 };
 
 export var sizeAduster: number = 1;

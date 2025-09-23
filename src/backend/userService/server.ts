a90cda3b5e5685ca5a875bf5bf11ee105f2d9957
Luuk Van Geffen <lvan-gef@f0r1s5.codam.nl>
2025-09-23 16:52:16 +0200

    Feat: skill issue

--- src/backend/userService/server.ts
+++ src/backend/userService/server.ts
@@ -63,7 +63,7 @@ class server {
         this.client = new OAuth2Client();
         this.db = new UserDatabase();
         this.validate = new Validator();
-        this.guestId = 3600
+        this.guestId = -3600
     }
 
     async init() {
@@ -192,7 +192,9 @@ class server {
         this.fastify.get('/me', async (req: FastifyRequest, reply: FastifyReply) => {
             if (req.session.user) {
                 let loggedIn = true;
+                console.error(req.session.user.userId, this.guestId)
                 if (req.session.user.userId < this.guestId) {
+                    console.error("alleen voor gasten")
                     loggedIn = false;
                 }
 
